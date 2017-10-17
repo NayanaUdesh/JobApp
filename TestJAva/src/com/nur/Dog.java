451acc02ee4706eb5243a1fde9453d7e79144697
Nayana Udesh Ranathunga <nayanaudesh@gmail.com>
2017-10-17 22:52:01 +0530

    New Project
    Tesing project

---
+++ TestJAva/src/com/nur/Dog.java
@@ -0,0 +1,7 @@
+package com.nur;
+
+public class Dog {
+	public void bark() {
+		System.out.println("Dog Barks....");
+	}
+}
---
+++ TestJAva/src/com/nur/Main.java
@@ -0,0 +1,11 @@
+package com.nur;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Dog german=new Dog();
+		german.bark();
+
+	}
+
+}
